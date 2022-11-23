cfc8f3033105d0fd82bb668bc21b83c6e0313702
HansikaPerera1117 <hansikaperera59@gmail.com>
2022-11-23 14:12:21 +0530

    update design of menu in game in js assignmnet in my portfolio

--- assignments/JS/Game/controllers/DashboardController.js
+++ assignments/JS/Game/controllers/DashboardController.js
@@ -6,10 +6,12 @@ $(window).on('load',function (){
 $(document).ready(function () {
 
     $("#menuMain").css('display','none');
+    $("#g").css('display','none');
 
     $("#btnPlay").click(function(){
         $("#startMain").css('display','none');
         $("#menuMain").css('display','block');
+        $("#g").css('display','none');
     });
 
     $("#menuMain").css('display','none');
@@ -17,6 +19,13 @@ $(document).ready(function () {
     $("#btnBackToHome").click(function(){
         $("#startMain").css('display','block');
         $("#menuMain").css('display','none');
+        $("#g").css('display','none');
+    });
+
+    $(".play").click(function(){
+        $("#startMain").css('display','none');
+        $("#menuMain").css('display','none');
+        $("#g").css('display','block');
     });
 
 });
